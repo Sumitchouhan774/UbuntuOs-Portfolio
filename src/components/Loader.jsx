ebf1f12a5a303b315fc9aa1d0171dfb2fe774ced
Sumitchouhan774 <sumitchouhan7743@gmail.com>
2025-02-23 11:55:30 +0530

    Update Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,10 +4,7 @@ function Loader() {
   return (
     <div className="loading-screen">
       <div className="spinner"></div>
-      <div className="logo">
-      <img className="loader" src="/Images/Logos/ubuntu-seeklogo.png" alt="Logo"/>
-      <h1 className="ubuntu-text">ubuntu</h1>
-      </div>
+      <img className="ubuntu-text" src="Images/Logos/ubuntu.webp" alt="logo"/>
     </div>
   );
 };
